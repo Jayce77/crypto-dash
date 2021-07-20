bb04474c5afb9a1ca559fb4aeae0390a67482ef0
Jayce77 <xjaycex@hotmail.com>
2021-07-19 21:20:10 -0400

    Local stroage setup, fixed intitial settings

--- src/App/AppProvider.js
+++ src/App/AppProvider.js
@@ -4,20 +4,28 @@ export const AppContext = React.createContext();
 
 export const AppProvider = props => {
   const savedSettings = () => {
-    let cryptoDash = JSON.parse(localStorage.getItem('cryptoDash'));
-    let settings = {};
-    if (cryptoDash) {
+    let cryptoDashData = JSON.parse(localStorage.getItem('cryptoDash'));
+    let settings = {page: 'dashboard', firstVisit: false};
+    if (!cryptoDashData) {
       settings = {page: 'settings', firstVisit: true};
     }
     return settings
   };
+
+  const settings = savedSettings();
   
-  const  [page, setPage] = useState();
-  const confirmFavorites = () => console.log('Confirmed');
+  const [page, setPage] = useState(settings.page);
+  const [firstVisit, setVisit ] = useState(settings.firstVisit);
+  const confirmFavorites = () => {
+    setVisit(false);
+    localStorage.setItem('cryptoDash', JSON.stringify({
+      test: 'hello'
+    }));
+  }
 
   return (
     <AppContext.Provider
-      value={{page, setPage, ...savedSettings, confirmFavorites}}
+      value={{page, firstVisit, setPage, confirmFavorites}}
     >
       {props.children}
     </AppContext.Provider>
--- src/Settings/WelcomeMessage.js
+++ src/Settings/WelcomeMessage.js
@@ -1,5 +1,13 @@
-const Welcome = props => (
-  <h1>Welcome to Crypto Dash</h1>
+import { AppContext } from "../App/AppProvider"
+
+const Welcome = ({firstVisit}) => (
+  <AppContext.Consumer>
+    { ({firstVisit}) => (
+      firstVisit && <div>Welcome to Crypto Dash, please select your favorite coins to begin</div>
+    )}
+  </AppContext.Consumer>
+
+  
 );
 
 export default Welcome
