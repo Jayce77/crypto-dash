6c5ef6c86495ee2fcc8cf99626a145ebf9cfa198
Jayce77 <xjaycex@hotmail.com>
2021-07-26 21:27:24 -0400

    added css auto-fill

--- src/App/AppProvider.js
+++ src/App/AppProvider.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
+import _ from 'lodash';
 
+const MAX_FAVORITES = 10;
 const CC = require('cryptocompare');
 CC.setApiKey(process.env.REACT_APP_CRYPTO_COMPARE_KEY);
 
@@ -8,11 +10,15 @@ export const AppContext = React.createContext();
 export const AppProvider = props => {
   const savedSettings = () => {
     let cryptoDashData = JSON.parse(localStorage.getItem('cryptoDash'));
+    const  defaultFavorites = ['BTC', 'ETH', 'ADA', 'MATIC', 'LINK'];
     let settings = {page: 'dashboard', firstVisit: false};
     if (!cryptoDashData) {
-      settings = {page: 'settings', firstVisit: true};
+      settings = {page: 'settings', firstVisit: true, favorites: defaultFavorites};
+    } else {
+      let {favorites} = cryptoDashData;
+      settings = {favorites, ...settings};
     }
-    return settings
+    return settings;
   };
 
   const settings = savedSettings();
@@ -20,13 +26,24 @@ export const AppProvider = props => {
   const [page, setPage] = useState(settings.page);
   const [firstVisit, setVisit ] = useState(settings.firstVisit);
   const [coinList, setCoinList] = useState();
+  const [favorites, setFavorites] = useState(settings.favorites);
 
   const confirmFavorites = () => {
     setVisit(false);
     localStorage.setItem('cryptoDash', JSON.stringify({
-      test: 'hello'
+      favorites: favorites
     }));
   }
+
+  const addCoin = (key) => {
+    if (favorites.length < MAX_FAVORITES) {
+      setFavorites([key, ...favorites]);
+    }
+  }
+
+  const removeCoin = (key) => setFavorites(_.without(favorites, key));
+
+  const isInFavorites = (key) => _.includes(favorites, key);
 
   useEffect(() => {
     const fetchCoins = async () => {
@@ -39,7 +56,18 @@ export const AppProvider = props => {
 
   return (
     <AppContext.Provider
-      value={{page, firstVisit, setPage, confirmFavorites, coinList}}
+      value={{
+        page,
+        firstVisit,
+        setPage,
+        confirmFavorites,
+        coinList,
+        favorites,
+        setFavorites,
+        addCoin,
+        removeCoin,
+        isInFavorites
+      }}
     >
       {props.children}
     </AppContext.Provider>
--- src/Settings/CoinGrid.js
+++ src/Settings/CoinGrid.js
@@ -1,22 +1,24 @@
-import styled, {css} from 'styled-components';
+import styled from 'styled-components';
 import {AppContext} from '../App/AppProvider';
 import CoinTile from './CoinTile';
 
 export const CoinGridStyled = styled.div`
   display: grid;
-  grid-template-columns: repeat(5, 1fr);
+  grid-template-columns: repeat(auto-fill, minmax(130px, 1fr));
   grid-gap: 15px;
   margin-top: 40px;
 `;
 
-const getCoinsToDisplay = (coinList, topSection) => 
-  Object.keys(coinList).slice(0, topSection ? 10 : 100);
+const getCoinsToDisplay = (coinList, topSection, favorites) => 
+  topSection 
+  ? favorites
+  : Object.keys(coinList).slice(0, 100);
 
 const CoinGrid = ({topSection}) => (
   <AppContext.Consumer>
-    {({coinList}) => (
+    {({coinList, favorites}) => (
       <CoinGridStyled>
-        {getCoinsToDisplay(coinList, topSection).map(coinKey =>
+        {getCoinsToDisplay(coinList, topSection, favorites).map(coinKey =>
           <CoinTile topSection={topSection} coinKey={coinKey} key={coinKey.Id} />)}
       </CoinGridStyled>
     )}
