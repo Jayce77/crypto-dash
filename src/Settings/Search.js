fffc5d0a975cfe516fee7e5e46d0d6038e8d7d91
Jayce Crowther <xjaycex@hotmail.com>
2021-07-29 21:27:55 -0400

    Added serch ui and style

---
+++ src/Settings/Search.js
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+import { backgroundColor2, fontSize2 } from '../Shared/Styles';
+
+const SearchGrid = styled.div`
+  display: grid;
+  grid-template-columns: 200px 1fr;
+`;
+
+const SearchInput = styled.input`
+  ${backgroundColor2};
+  ${fontSize2};
+  color: #1163C9;
+  border: 1px solid;
+  height: 25px;
+  place-self: center left;
+`;
+
+const Search = () => (
+  <SearchGrid>
+    <h2>Search All Coins</h2>
+    <SearchInput />
+  </SearchGrid>
+)
+
+export default Search;
--- src/Settings/index.js
+++ src/Settings/index.js
@@ -2,12 +2,14 @@ import ConfirmButton from './ConfirmButton';
 import WelcomeMessage from './WelcomeMessage';
 import Page from '../Shared/Page';
 import CoinGrid from './CoinGrid';
+import Search from './Search';
 
 const Settings = () => (
   <Page name="settings">
     <WelcomeMessage />
     <CoinGrid topSection />
     <ConfirmButton />
+    <Search />
     <CoinGrid />
   </Page>
 );
